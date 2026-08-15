338f780656e8838f77fcb31b5be46ea36689898e
Wendel <wende08@proton.me>
2026-08-15 14:17:34 -0300

    removed linux-pytuna from /etc/mkinitcpio.d

--- profiledef.sh
+++ profiledef.sh
@@ -15,13 +15,13 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/shadow"]="0:0:0400"
   ["/etc/gshadow"]="0:0:0400"
+  ["/root"]="0:0:750"
+  ["/root/.gnupg"]="0:0:700"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/nvidia-module-loader"]="0:0:755"
+  ["/usr/local/bin/pkexec-wrapper"]="0:0:755"
+  ["/usr/local/bin/remove-nvidia"]="0:0:755"
 )
